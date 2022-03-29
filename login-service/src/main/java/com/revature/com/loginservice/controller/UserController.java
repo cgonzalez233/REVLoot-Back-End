f46e7d081da5f8420a7a6a767ec52a041c3f4db2
MolSnoo <molsno.vivian@gmail.com>
2022-03-29 14:13:55 -0400

    Removed unneeded import

--- login-service/src/main/java/com/revature/com/loginservice/controller/UserController.java
+++ login-service/src/main/java/com/revature/com/loginservice/controller/UserController.java
@@ -5,7 +5,6 @@ import com.revature.com.loginservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
 import java.util.List;
 
 @RestController
